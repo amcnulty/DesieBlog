274fd5474ca7398e5b4ed71db7d190cf7c37812c
amcnulty <amcnulty88@swbell.net>
2019-05-11 11:20:42 -0700

    Header Redesign
    Redesigned the header per the new design specifications.

--- client/src/header/header.js
+++ client/src/header/header.js
@@ -4,88 +4,11 @@ import HeaderSearch from './headerSearch/headerSeach';
 import { Nav, NavItem, Collapse, Navbar, NavbarToggler, NavLink } from 'reactstrap';
 import './header.css';
 
-const headerInitialHeight = 190;
-const headerInitialHeightMobile = 160;
-const headerHeightDelta = 70;
-const headerHeightDeltaMobile = 100;
-const logoInitialHeight = 100;
-const logoInitialHeightMobile = 80;
-const logoHeightDelta = 55;
-const logoHeightDeltaMobile = 35;
-const logoTopInitial = 65;
-const animationScrollRange = 300;
-
 class SiteHeader extends Component {
 
   constructor(props) {
     super(props);
-    if (window.innerWidth < 768) {
-      this.state = {
-        headerHeight: '160px',
-        logoHeight: '80px',
-        logoTop: '65px'
-      }
-    }
-    else {
-      this.state = {
-        headerHeight: '190px',
-        logoHeight: '100px',
-        logoTop: '65px'
-      }
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll = () => {
-    let headerHeight;
-    let headerDelta;
-    let logoHeight;
-    let logoDelta;
-    let topDelta;
-    if (window.innerWidth < 768) {
-      headerHeight = headerInitialHeightMobile;
-      headerDelta = headerHeightDeltaMobile;
-      logoHeight = logoInitialHeightMobile;
-      logoDelta = logoHeightDeltaMobile;
-      topDelta = 55;
-    }
-    else {
-      headerHeight = headerInitialHeight;
-      headerDelta = headerHeightDelta;
-      logoHeight = logoInitialHeight;
-      logoDelta = logoHeightDelta;
-      topDelta = 0;
-    }
-    if (window.pageYOffset < 301) {
-      this.setState({
-        headerHeight: `${headerHeight - (window.pageYOffset / animationScrollRange) * headerDelta}px`,
-        logoHeight: `${logoHeight - (window.pageYOffset / animationScrollRange) * logoDelta}px`,
-        logoTop: `${logoTopInitial - (window.pageYOffset / animationScrollRange) * topDelta}px`
-      });
-    }
-    else {
-      if (window.innerWidth < 768) {
-        this.setState({
-          headerHeight: headerHeight - headerDelta + 'px',
-          logoHeight: logoHeight - logoDelta + 'px',
-          logoTop: logoTopInitial - topDelta
-        });
-      }
-      else {
-        this.setState({
-          headerHeight: headerHeight - headerDelta + 'px',
-          logoHeight: logoHeight - logoDelta + 'px',
-          logoTop: logoTopInitial + 'px'
-        });
-      }
-    }
+      this.state = {}
   }
 
   checkIfActive = path => {
@@ -99,45 +22,47 @@ class SiteHeader extends Component {
   render() {
     
     return (
-      <header
-        className="SiteHeader"
-        style={{height: this.state.headerHeight}}
-      >
+      <header className="SiteHeader">
         <Navbar light expand="md">
           <NavbarToggler onClick={() => this.toggle('mobileNavbarIsOpen')}/>
-          <a href="/">
-            <img
-              className="desieBlogLogo"
-              src="/res/images/DesieBlog_Logo.png"
-              alt="DesieBlog"
-              style={{height: this.state.logoHeight, top: this.state.logoTop}}
-            />
-          </a>
           <Collapse isOpen={this.state.mobileNavbarIsOpen} navbar>
             <Nav className="mr-auto" navbar>
-              <NavItem className="mr-auto">
+              <NavItem>
+                <NavLink className={this.checkIfActive('/') ? 'active' : ''} href="/">
+                  HOME
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className={this.checkIfActive('/contact-us') ? 'active' : ''} href="/about">
+                  ABOUT
+                </NavLink>
+              </NavItem>
+              <NavItem>
                 <NavLink className={this.checkIfActive('/recipes') ? 'active' : ''} href="/recipes">
-                  Recipes
+                  RECIPES
                 </NavLink>
               </NavItem>
-              <NavItem className="mr-auto">
-                <NavLink className={this.checkIfActive('/wine') ? 'active' : ''} href="/wine">
-                  Wine
-                </NavLink>
-              </NavItem>
-              <NavItem className="mr-auto">
+              <NavItem>
                 <NavLink className={this.checkIfActive('/travel') ? 'active' : ''} href="/travel">
-                  Travel
+                  TRAVEL
                 </NavLink>
               </NavItem>
-              <NavItem className="mr-auto">
+              <NavItem>
+                <NavLink className={this.checkIfActive('/wine') ? 'active' : ''} href="/wine">
+                  WINE
+                </NavLink>
+              </NavItem>
+              <NavItem>
                 <NavLink className={this.checkIfActive('/books') ? 'active' : ''} href="/books">
-                  Books
+                  BOOKS
                 </NavLink>
               </NavItem>
             </Nav>
-            <HeaderSearch/>
           </Collapse>
+          <NavLink className="pintrestIcon ml-auto" href="https://www.pinterest.com/desieblog/" target="_blank">
+            <i className="fab fa-pinterest"></i>
+          </NavLink>
+          <HeaderSearch/>
         </Navbar>
       </header>
     )
--- client/src/header/headerSearch/headerSeach.js
+++ client/src/header/headerSearch/headerSeach.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Modal, ModalBody, ModalHeader, ModalFooter, Button } from 'reactstrap';
 import './headerSearch.css';
 
-const HeaderSearch = props => {
+class HeaderSearch extends Component {
 
-  return (
-    <form className="HeaderSearch">
-      <input type="text" placeholder="Search..." name="search" id="searchInput"/>
-      <button type="submit" className="db-button"><i className="fa fa-search"></i></button>
-    </form>
-  )
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    }
+  }
+  
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    return (
+      <div className="HeaderSearch">
+        <button
+          className="db-button"
+          type="submit"
+          onClick={this.toggle}
+        >
+          <i className="fa fa-search"></i>
+        </button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}><i className="fas fa-exclamation-triangle"></i> Information</ModalHeader>
+          <ModalBody>
+            <label htmlFor="searchBar">Search for an article</label>
+            <input id="searchBar" className="form-control" type="text" placeholder="Type search here..."/>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="link" onClick={this.toggle}>CANCEL</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    )
+  }
 }
 
 export default HeaderSearch;
